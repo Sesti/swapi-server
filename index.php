20c657edb33b788d7b893b05139b5fc33531a019
Sébastian Pilon <sebastian.pilon@gmail.com>
2020-10-26 09:56:14 -0400

    Added search route

--- index.php
+++ index.php
@@ -7,114 +7,152 @@ define('API_URL', 'https://swapi.dev/api/');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, origin");
 
-Flight::route('/api/v1/people', function () {
-    $request = @json_decode(file_get_contents(API_URL . "people/"));
+Flight::route('/api/v1/people(/@page)', function ($page = 1) {
+    $request = @json_decode(file_get_contents(API_URL . "people/?page=$page"));
 
-    $persons = array_map(function ($people) {
-
-        $person = (object) [
-            'name' => $people->name,
-            'gender' => $people->gender,
-            'homeworld' => $people->homeworld,
-            'starships' => $people->starships,
+    if ($request == null) {
+        $response = (object) [
+            'status' => 'none'
         ];
-        return $person;
-    }, $request->results);
+    } else {
 
-    $response = (object) [
-        'count' => $request->count,
-        'next' => $request->next,
-        'previous' => $request->previous,
-        'persons' => $persons
-    ];
+        $persons = array_map(function ($people) {
 
-    //echo $response;
-    Flight::json($response);
-});
+            $person = (object) [
+                'name' => $people->name,
+                'gender' => $people->gender,
+                'homeworld' => $people->homeworld,
+                'starships' => $people->starships,
+                'hairColor' => $people->hair_color,
+                'skinColor' => $people->skin_color,
+                'eyeColor' => $people->eye_color,
+            ];
+            return $person;
+        }, $request->results);
 
-Flight::route('/api/v1/search', function () {
-    $query = $_GET['s'];
-
-    if (!isset($query) || empty($query)) {
-        Flight::json((object) ['count' => 0]);
-        return;
+        $response = (object) [
+            'count' => $request->count,
+            'next' => $request->next,
+            'previous' => $request->previous,
+            'persons' => $persons
+        ];
     }
 
-    $request = @json_decode(file_get_contents(API_URL . "people/?search=" . $query));
+    Flight::json($response);
+});
 
-    if (!$request->count) {
-        Flight::json((object) ['count' => 0]);
-        return;
+Flight::route('/api/v1/search(/@name)', function ($name) {
+
+    if (empty($name)) {
+        $response = (object) [
+            'status' => 'none'
+        ];
+    } else {
+
+        $request = @json_decode(file_get_contents(API_URL . "people/?search=$name"));
+
+        if (isset($request->count) && $request->count === 0) {
+            $response = (object) [
+                'status' => 'none'
+            ];
+        } else {
+
+            $persons = array_map(function ($people) {
+
+                $person = (object) [
+                    'name' => $people->name,
+                    'gender' => $people->gender,
+                    'homeworld' => $people->homeworld,
+                    'starships' => $people->starships,
+                    'hairColor' => $people->hair_color,
+                    'skinColor' => $people->skin_color,
+                    'eyeColor' => $people->eye_color,
+                ];
+                return $person;
+            }, $request->results);
+
+            $response = (object) [
+                'count' => $request->count,
+                'next' => $request->next,
+                'previous' => $request->previous,
+                'persons' => $persons
+            ];
+        }
     }
 
-    $persons = array_map(function ($people) {
-
-        $person = (object) [
-            'name' => $people->name,
-            'gender' => $people->gender,
-            'homeworld' => $people->homeworld,
-            'starships' => $people->starships,
-        ];
-        return $person;
-    }, $request->results);
-
-    $response = (object) [
-        'count' => $request->count,
-        'next' => $request->next,
-        'previous' => $request->previous,
-        'persons' => $persons
-    ];
-
-    //echo $response;
     Flight::json($response);
 });
 
-Flight::route('/api/v1/planets', function () {
-    $request = @json_decode(file_get_contents(API_URL . "planets/"));
+Flight::route('/api/v1/planets(/@page)', function ($page = 1) {
+    $request = @json_decode(file_get_contents(API_URL . "planets/?page=$page"));
 
-    $planets = array_map(function ($planet) {
-
-        $planet = (object) [
-            'name' => $planet->name,
-            'diameter' => $planet->diameter,
-            'gravity' => $planet->gravity,
+    if ($request == null) {
+        $response = (object) [
+            'status' => 'none'
         ];
-        return $planet;
-    }, $request->results);
+    } else {
+        $planets = array_map(function ($planet) {
 
-    $response = (object) [
-        'count' => $request->count,
-        'next' => $request->next,
-        'previous' => $request->previous,
-        'planets' => $planets
-    ];
+            $planet = (object) [
+                'name' => $planet->name,
+                'diameter' => $planet->diameter,
+                'gravity' => $planet->gravity,
+                'climate' => $planet->climate,
+                'population' => $planet->population
+            ];
+            return $planet;
+        }, $request->results);
 
-    //echo $response;
+        $response = (object) [
+            'count' => $request->count,
+            'next' => $request->next,
+            'previous' => $request->previous,
+            'planets' => $planets
+        ];
+    }
+
     Flight::json($response);
 });
 
-Flight::route('/api/v1/starships', function () {
-    $request = @json_decode(file_get_contents(API_URL . "starships/"));
+Flight::route('/api/v1/starships(/@page)', function ($page = 1) {
+    $request = @json_decode(file_get_contents(API_URL . "starships/?page=$page"));
 
-    $starships = array_map(function ($starship) {
-
-        $starship = (object) [
-            'name' => $starship->name,
-            'model' => $starship->model,
-            'starship_class' => $starship->starship_class,
+    if ($request == null) {
+        $response = (object) [
+            'status' => 'none'
         ];
-        return $starship;
-    }, $request->results);
+    } else {
+        $starships = array_map(function ($starship) {
 
-    $response = (object) [
-        'count' => $request->count,
-        'next' => $request->next,
-        'previous' => $request->previous,
-        'starships' => $starships
+            $starship = (object) [
+                'name' => $starship->name,
+                'model' => $starship->model,
+                'starship_class' => $starship->starship_class,
+            ];
+            return $starship;
+        }, $request->results);
+
+        $response = (object) [
+            'count' => $request->count,
+            'next' => $request->next,
+            'previous' => $request->previous,
+            'starships' => $starships
+        ];
+    }
+
+    Flight::json($response);
+});
+
+Flight::route('/api/v1/starship/@id', function ($id) {
+    $request = @json_decode(file_get_contents(API_URL . "starships/$id"));
+
+    $starship = (object) [
+        'name' => $request->name,
+        'model' => $request->model,
+        'starship_class' => $request->starship_class,
     ];
 
-    //echo $response;
-    Flight::json($response);
+    Flight::json($starship);
 });
 
 Flight::start();
